c891798b27bb3d83dec90d31ae53163c36a6d46a
MeichengWang <56236589+MeichengWang@users.noreply.github.com>
2019-10-07 01:25:48 -0400

    Update reducer1.py

--- reducer1.py
+++ reducer1.py
@@ -4,16 +4,20 @@
 # In[ ]:
 from operator import itemgetter
 import sys
+
 dict_ip_count = {}
+
 for line in sys.stdin:
     line = line.strip()
-    ip, num = line.split('\t')
+    ip,num = line.split('\t')
     try:
         num = int(num)
         dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
+
     except ValueError:
         pass
-sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
-for ip, count in sorted_dict_ip_count:
-        print ('%s\t%s' % (ip, count))
 
+
+sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(1),reverse=True)[:3]
+for ip, count in sorted_dict_ip_count:
+     print ('%s\t%s' % (ip, count))
